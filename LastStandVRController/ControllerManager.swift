7b9d19bfe1ee27743488185abf67a9e7d1cfb6a4
Rohin Tangirala <rohinkt@gmail.com>
2017-08-07 10:38:34 -0700

    formatting

--- LastStandVRController/ControllerManager.swift
+++ LastStandVRController/ControllerManager.swift
@@ -1,43 +1,43 @@
 //
-//  ControllerManager.swift
-//  LastStandVRController
+//    ControllerManager.swift
+//    LastStandVRController
 //
-//  Created by Rohin Tangirala on 3/25/17.
-//  Copyright © 2017 Rohin Tangirala. All rights reserved.
+//    Created by Rohin Tangirala on 3/25/17.
+//    Copyright © 2017 Rohin Tangirala. All rights reserved.
 //
 
 import Foundation
 
 class ControllerManager {
-  let redisServer : Redis = Redis()
-  let ip : String = "10.10.180.94"
-  let port : Int = 6379
+    let redisServer : Redis = Redis()
+    let ip : String = "10.10.180.94"
+    let port : Int = 6379
 
-  var ct : Int = 0
+    var ct : Int = 0
 
-  class var sharedInstance: ControllerManager {
-    struct Static {
-      static let instance = ControllerManager()
+    class var sharedInstance: ControllerManager {
+        struct Static {
+            static let instance = ControllerManager()
+        }
+        
+        return Static.instance
+    }
+
+    init() {
+        connect()
+        redisServer.Command(Command: "SET appPlatform \"iOS\"")
     }
     
-    return Static.instance
-  }
-
-  init() {
-    connect()
-    redisServer.Command(Command: "SET appPlatform \"iOS\"")
-  }
-  
-  private func connect() {
-    redisServer.server(endPoint: ip, onPort: UInt16(port))
-  }
-  
-  func sendAngle() {
-    redisServer.Command(Command: "SET xRot \(phoneX)")
-    redisServer.Command(Command: "SET zRot \(phoneZ)")
-  }
-  
-  func sendFire() {
-    redisServer.Command(Command: "SET fire 1")
-  }
+    private func connect() {
+        redisServer.server(endPoint: ip, onPort: UInt16(port))
+    }
+    
+    func sendAngle() {
+        redisServer.Command(Command: "SET xRot \(phoneX)")
+        redisServer.Command(Command: "SET zRot \(phoneZ)")
+    }
+    
+    func sendFire() {
+        redisServer.Command(Command: "SET fire 1")
+    }
 }
--- LastStandVRController/ViewController.swift
+++ LastStandVRController/ViewController.swift
@@ -1,9 +1,9 @@
 //
-//  ViewController.swift
-//  LastStandVRController
+//    ViewController.swift
+//    LastStandVRController
 //
-//  Created by Rohin Tangirala on 3/25/17.
-//  Copyright © 2017 Rohin Tangirala. All rights reserved.
+//    Created by Rohin Tangirala on 3/25/17.
+//    Copyright © 2017 Rohin Tangirala. All rights reserved.
 //
 
 import UIKit
@@ -14,43 +14,43 @@ var phoneY : Double = 0.0
 var phoneZ : Double = 0.0
 
 class ViewController: UIViewController {
-  var motionManager: CMMotionManager!
-  var initialX : Double = 0.0
-  var initialY : Double = 0.0
-  var initialZ : Double = 0.0
-  
-  @IBAction func calibrate(_ sender: Any) {
-    initialX = phoneX
-    initialY = phoneY
-    initialZ = phoneZ
-  }
-  
-  @IBAction func fireButton(_ sender: Any) {
-    ControllerManager.sharedInstance.sendFire()
-  }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Do any additional setup after loading the view, typically from a nib.
-    motionManager = CMMotionManager()
-    motionManager.deviceMotionUpdateInterval = TimeInterval(exactly: 0.25)!
-
-    if motionManager.isDeviceMotionAvailable == true {
-      motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler:{
-        data, error in
-
-        phoneX = -(data?.attitude.yaw)! - self.initialX
-        phoneY = (data?.attitude.roll)! - self.initialY
-        phoneZ = (data?.attitude.pitch)! - self.initialZ
-        ControllerManager.sharedInstance.sendAngle()
-      })
+    var motionManager: CMMotionManager!
+    var initialX : Double = 0.0
+    var initialY : Double = 0.0
+    var initialZ : Double = 0.0
+    
+    @IBAction func calibrate(_ sender: Any) {
+        initialX = phoneX
+        initialY = phoneY
+        initialZ = phoneZ
     }
-  }
+    
+    @IBAction func fireButton(_ sender: Any) {
+        ControllerManager.sharedInstance.sendFire()
+    }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+        motionManager = CMMotionManager()
+        motionManager.deviceMotionUpdateInterval = TimeInterval(exactly: 0.25)!
+
+        if motionManager.isDeviceMotionAvailable == true {
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler:{
+                data, error in
+
+                phoneX = -(data?.attitude.yaw)! - self.initialX
+                phoneY = (data?.attitude.roll)! - self.initialY
+                phoneZ = (data?.attitude.pitch)! - self.initialZ
+                ControllerManager.sharedInstance.sendAngle()
+            })
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
